131e5cfaed7c7edc34cf8b21ef0b1efd74b920e5
DaniMiyuki <40705330+DaniMiyuki@users.noreply.github.com>
2023-06-22 00:28:44 +0900

    Calculadora finalizada
    correcao da opcao 0 para sair

--- AP_linguagem_de_programacao/main.py
+++ AP_linguagem_de_programacao/main.py
@@ -7,7 +7,8 @@ class Calculadora:
 
     @staticmethod
     def opcoes_do_menu():
-        print("Menu")
+        print("*"*10 +"Menu" + "*"*10)
+        print("~"*24)
         print("[1] Adicao")
         print("[2] Subtracao")
         print("[3] Multiplicacao")
@@ -23,8 +24,11 @@ class Calculadora:
                 opcao_escolhida = input("Escolha uma opcao: ")
                 try:
                     opcao_escolhida = int(opcao_escolhida)
-                    if 0 <= opcao_escolhida <= 7:
+                    if 0 < opcao_escolhida <= 7:
                         return opcao_escolhida
+                    elif opcao_escolhida == 0:
+                         print("Saindo do programa...")
+                         exit()
                     else:
                          print("Opcao invalida. Digite um numero entre 0 e 7.")
                 except ValueError:
@@ -32,25 +36,25 @@ class Calculadora:
 
     @staticmethod
     def calcular(opcao_escolhida, num1, num2):
-         if opcao_escolhida == 1:
-              return num1 + num2
-         elif opcao_escolhida == 2:
-              return num1 - num2
-         elif opcao_escolhida == 3:
-              return num1 * num2
-         elif opcao_escolhida == 4:
-              return num1 / num2
-         elif opcao_escolhida == 5:
-              return num1 ** num2
-         elif opcao_escolhida == 6:
-              return math.sqrt(num1 + num2)
-         elif opcao_escolhida == 7:
-              return num1 % num2
-         elif opcao_escolhida == 0:
-              print("Saindo do programa...")
-              exit()
-         else:
-              print("Opcao invalida.Tente novamente!!.")
+         while True:
+          if opcao_escolhida == 1:
+               return num1 + num2
+          elif opcao_escolhida == 2:
+               return num1 - num2
+          elif opcao_escolhida == 3:
+               return num1 * num2
+          elif opcao_escolhida == 4:
+               return num1 / num2
+          elif opcao_escolhida == 5:
+               return num1 ** num2
+          elif opcao_escolhida == 6:
+               return math.sqrt(num1 + num2)
+          elif opcao_escolhida == 7:
+               return num1 % num2
+          elif opcao_escolhida == 0:
+               exit()
+          else:
+               print("Opcao invalida.Tente novamente!!.")
 
 
 
@@ -60,7 +64,7 @@ while True:
     ru1 = int(input("Digite o PENULTIMO No do seu RU: "))
     ru2 = int(input("Digite o ULTIMO No do seu RU: "))
     resultado = Calculadora.calcular(escolha, ru1, ru2)
-    print(f"Resultado:{resultado:.2f} ")
+    print(f"Resultado:{resultado} ")
     print()
     pergunta = input("deseja continuar? [s]sim ou[n]nao: ").lower().strip()
     if pergunta == "s":
@@ -68,4 +72,3 @@ while True:
     else:
          print("Programa encerrado!!")
          exit()
-
